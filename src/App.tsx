b47fe18153d3596457653776458cccde922f5f62
YashParekh1301 <yashparekh1301@gmail.com>
2022-07-27 23:34:19 +0530

    added route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import CovidContainer from "./container/CovidContainer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <CovidContainer />
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<CovidContainer />}>
+          <Route path="*" element={<CovidContainer />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
